247cae89c7b5b18f2f6449c4f5c7eae96a4e0248
ABS <artemb7555@gmail.com>
2026-09-02 09:35:59 +0300

    Initial working SDL3 OpenGL client

---
+++ src/Input/InputState.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct InputState
+{
+    float mouseX = 0.0f;
+    float mouseY = 0.0f;
+
+    float mouseDeltaX = 0.0f;
+    float mouseDeltaY = 0.0f;
+
+    bool leftButton = false;
+    bool rightButton = false;
+};
